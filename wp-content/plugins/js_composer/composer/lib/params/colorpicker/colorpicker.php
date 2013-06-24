c8d954f24fd6388600cfd7b5bbf355cd76a3c5d9
Martin Vernon <martin.vernon@gmail.com>
2013-06-24 13:31:00 +0100

    js_composer issues

--- wp-content/plugins/js_composer/composer/lib/params/colorpicker/colorpicker.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function vc_colorpicker_form_field($settings, $value) {
-    return '<div class="color-group">'
-                .'<input name="'.$settings['param_name'].'" class="wpb_vc_param_value wpb-textinput '.$settings['param_name'].' '.$settings['type'].'_field" type="text" value="'.$value.'"/>'
-                .'<div class="vc-color-picker-block"></div>'
-            .'</div>';
-}
