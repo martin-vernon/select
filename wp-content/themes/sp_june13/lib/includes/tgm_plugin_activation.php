bbdb2c17709ed5e7568933322f62bb458ea4177e
Martin Vernon <martin.vernon@gmail.com>
2013-06-27 17:42:59 +0100

    Removed Google Analytics plugin
    Removed Google Analytics plugin, moved the analytics ID to the roots
    config file

--- wp-content/themes/sp_june13/lib/includes/tgm_plugin_activation.php
+++ wp-content/themes/sp_june13/lib/includes/tgm_plugin_activation.php
@@ -57,15 +57,8 @@ function my_theme_register_required_plugins() {
 			'force_deactivation'                    => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
 		),
                 array(
-			'name'     				=> 'BootstraPress', // The plugin name
-			'slug'     				=> 'bootsrapress', // The plugin slug (typically the folder name)
-			'required' 				=> true, // If false, the plugin is only 'recommended' instead of required
-			'force_activation'                      => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
-			'force_deactivation'                    => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
-		),
-                array(
-			'name'     				=> 'Google Analytics', // The plugin name
-			'slug'     				=> 'googleanalytics', // The plugin slug (typically the folder name)
+			'name'     				=> 'BootstrapPress', // The plugin name
+			'slug'     				=> 'bootstrapress', // The plugin slug (typically the folder name)
 			'required' 				=> true, // If false, the plugin is only 'recommended' instead of required
 			'force_activation'                      => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
 			'force_deactivation'                    => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
